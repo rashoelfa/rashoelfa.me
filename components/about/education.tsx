59be11c25e15ea685380ecd89990d8f3bea56dc5
rashoelfa <rashoelfa@gmail.com>
2022-09-16 17:17:19 +0700

    fix: remove ipk

--- components/about/education.tsx
+++ components/about/education.tsx
@@ -10,7 +10,6 @@ export default function Education() {
         <h2 className="text-lg font-semibold">Institut Teknologi PLN</h2>
         <p>S1 - Teknik Informatika</p>
         <p>2018 - Aug 2022</p>
-        <p>IPK : 3.83</p>
       </div>
     </>
   );

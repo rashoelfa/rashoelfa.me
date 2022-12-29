beb62e20d799d651b4cdad512bbb43bcffb6e9b2
rashoelfa <rashoelfa@gmail.com>
2022-12-29 11:02:41 +0700

    fix: revert @next/font because available on v13 and fix logo alt

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,35 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import NextNProgress from "nextjs-progressbar";
-import localFont from "@next/font/local";
-
-const font = localFont({
-  src: [
-    {
-      path: "./fonts/SFPRODISPLAYREGULAR.OTF",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/SFPRODISPLAYMEDIUM.OTF",
-      weight: "500",
-      style: "medium",
-    },
-    {
-      path: "./fonts/SFPRODISPLAYBOLD.OTF",
-      weight: "700",
-      style: "bold",
-    },
-  ],
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <NextNProgress color="#000" options={{ showSpinner: false }} />
-      <main className={font.className}>
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }

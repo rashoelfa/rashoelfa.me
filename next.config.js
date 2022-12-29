f9d80812638c40d7f3823dbe1415e77b43900dc6
rashoelfa <rashoelfa@gmail.com>
2022-12-29 10:58:40 +0700

    fix: hide css class

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
+const path = require("path");
+const loaderUtils = require("loader-utils");
+
+const hashOnlyIdent = (context, _, exportName) =>
+  loaderUtils
+    .getHashDigest(
+      Buffer.from(
+        `filePath:${path
+          .relative(context.rootContext, context.resourcePath)
+          .replace(/\\+/g, "/")}#className:${exportName}`
+      ),
+      "md4",
+      "base64",
+      6
+    )
+    .replace(/[^a-zA-Z0-9-_]/g, "_")
+    .replace(/^(-?\d|--)/, "_$1");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -5,7 +23,25 @@ const nextConfig = {
   images: {
     domains: ["countryflagsapi.com"],
   },
-  output: 'standalone',
+  output: "standalone",
+  webpack(config, { dev }) {
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === "object")
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    if (!dev)
+      rules.forEach((rule) => {
+        rule.use.forEach((moduleLoader) => {
+          if (
+            moduleLoader.loader?.includes("css-loader") &&
+            !moduleLoader.loader?.includes("postcss-loader")
+          )
+            moduleLoader.options.modules.getLocalIdent = hashOnlyIdent;
+        });
+      });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;

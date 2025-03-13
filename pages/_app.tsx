e5f51e7127369603b5f5e26ff5463da01be3fcd2
rashoelfa <rashoelfa@gmail.com>
2025-03-13 14:55:50 +0700

    feat: add custom cursor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import NextNProgress from "nextjs-progressbar";
+import FluidCursor from "../components/fluid-cursor";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <NextNProgress color="#000" options={{ showSpinner: false }} />
-      <Component {...pageProps} />
+      <Component {...pageProps}></Component>
+      <FluidCursor />
     </ThemeProvider>
   );
 }
